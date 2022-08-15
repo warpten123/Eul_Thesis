556a81bd0c280e29aea5b7eaaf7805f28f800e7d
Cristopher Bohol <cris.bacus6@gmail.com>
2022-08-15 21:57:00 +0800

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:thesis_eul/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
